0fba20018109408af001524ae053f800acb28cf4
Tassanai <amm_25204@hotmail.com>
2018-08-07 00:05:52 +0700

    เพิ้ม structor

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        phpinfo();
+        $minute = 60;
+        $text = Cache::remember('test', $minute, function () {
+            return 'Aumkung Studio';
+        });
+        return view('home', [
+            'text' => $text
+        ]);
     }
 }
